5f1c91b90680afea591c330bf1087fd18028ce70
“Gupta049” <sushilgupta049@gmail.com>
2023-10-14 14:11:12 +0530

    Adding Welcome java File to the master

---
+++ WelcomeToLineComparison.java
@@ -0,0 +1,7 @@
+/*Welcome to Line Comparison Computation Program on Master Branch*/
+
+public class WelcomeToLineComparison {
+	public static void main(String[] args){
+		System.out.println("Welcome To Line Comparison Program");
+	}
+}
