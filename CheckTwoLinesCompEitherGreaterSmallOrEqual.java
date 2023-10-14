056931fba9560397271823ea26b114c44bce085e
“Gupta049” <sushilgupta049@gmail.com>
2023-10-14 23:32:49 +0530

    Calculating either greater, smaller or equal line

---
+++ CheckTwoLinesCompEitherGreaterSmallOrEqual.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class CheckTwoLinesCompEitherGreaterSmallOrEqual{
+        public static void main(String[] args){
+                Scanner sc = new Scanner(System.in);
+                System.out.println("Enter the cordinate value for first line");
+                System.out.print("Enter the x1 co-ordinate of first point ");
+				double x1=sc.nextDouble();
+				System.out.print("Enter the y1 co-ordinate of first point ");
+                double y1=sc.nextDouble();
+				System.out.print("Enter the x2 co-ordinate of Second point ");
+                double x2=sc.nextDouble();
+				System.out.print("Enter the y2 co-ordinate of Second point ");
+                double y2=sc.nextDouble();
+				double lengthOfFirstLine = Math.sqrt(Math.pow((x2-x1), 2) + Math.pow((y2-y1), 2));
+				System.out.println("Calculate the length for first line is "+lengthOfFirstLine);
+				String l1 = String.valueOf(lengthOfFirstLine);
+				System.out.println("Enter the cordinate value for Second line");
+                System.out.print("Enter the X1 co-ordinate of first point ");
+				double X1=sc.nextDouble();
+				System.out.print("Enter the Y1 co-ordinate of first point ");
+                double Y1=sc.nextDouble();
+				System.out.print("Enter the X2 co-ordinate of Second point ");
+                double X2=sc.nextDouble();
+				System.out.print("Enter the Y2 co-ordinate of Second point ");
+                double Y2=sc.nextDouble();
+				double lengthOfSecondLine = Math.sqrt(Math.pow((X2-X1), 2) + Math.pow((Y2-Y1), 2));
+                String l2 = String.valueOf(lengthOfSecondLine);
+				System.out.println("Calculate the length for second line is " + lengthOfSecondLine);
+				if(l1.compareTo(l2) == 0){
+                        System.out.println("two line are equal ");
+                }else if(l1.compareTo(l2) > 0){
+                        System.out.println("First Line is greater ");
+                }else if(l1.compareTo(l2) < 0){
+						System.out.println("Second Line is greater ");
+				}
+        }
+}
